43622c2b57770e99b76f1a02d70ce90d6b0ea324
Khazene <158053347+Khazene@users.noreply.github.com>
2026-06-06 16:07:15 +0100

    Update pro.js

--- pro.js
+++ pro.js
@@ -1,3 +1,13 @@
+
+
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key))) {
+    e.preventDefault();
+  }
+});
+
 // ==========================================
 // 1. DONNÉES ET CONFIGURATION DES CORPS
 // ==========================================
